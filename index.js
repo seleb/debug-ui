494d1a27edbf84d5f34e09c6c3e6d1ef70d2d6f5
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-03-18 03:07:01 -0400

    better debug ui

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ document.body.appendChild(debugEl);
 
 export function init() {
 	function update() {
-		Object.values(debug).forEach(({ input, getter }) => {
-			input.value = getter();
+		Object.values(debug).forEach(({ update }) => {
+			update();
 		});
 	}
 	function updateLoop() {
@@ -24,15 +24,62 @@ export function init() {
 export function debugValue(name, getter, setter) {
 	const el = document.createElement('li');
 	const label = document.createElement('label');
+	const text = document.createElement('span');
+	text.textContent = name;
+	label.appendChild(text);
+	let update;
+	const initial = getter();
+	const type = typeof initial;
+	if (type === 'object') {
+		// TODO: object debugger
+		// also null
+		return;
+	}
+	let prev;
 	const input = document.createElement('input');
-	el.appendChild(label);
-	label.textContent = name;
+	if (type === 'number') {
+		input.type = 'number';
+		input.onchange = ({ currentTarget: { value } }) => setter(parseFloat(value));
+		update = current => input.value = current;
+		if (!setter) {
+			input.readOnly = input.disabled = true;
+		}
+	} else if(type === 'boolean') {
+		input.type = 'checkbox';
+		input.onchange = ({ currentTarget: { checked } }) => setter(checked);
+		update = current => input.checked = current;
+		if (!setter) {
+			input.readOnly = input.disabled = true;
+		}
+	} else if (type === 'function') {
+		text.remove();
+		input.type = 'button';
+		input.value = name;
+		input.onclick = () => getter()();
+		update = current => {};
+	} else if (type === 'string') {
+		input.type = 'text';
+		input.onchange = ({ currentTarget: { value } }) => setter(value);
+		update = current => input.value = current;
+		if (!setter) {
+			input.readOnly = input.disabled = true;
+		}
+	} else {
+		throw new Error(`Cannot debug "${name}" due to unsupported type "${type}"`);
+	}
+	update(initial);
 	label.appendChild(input);
+	el.appendChild(label);
 
 	const entry = debug[name] = {
-		getter,
-		setter,
-		input,
+		update: () => {
+			const current = getter();
+			if (prev === current) {
+				return;
+			}
+			update(current);
+			prev = current;
+		},
 	};
 	listEl.appendChild(el);
 }

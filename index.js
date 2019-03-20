b6ea640b552c4d248c84f4a44373dbb8f4700c94
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-03-20 01:55:39 -0400

    obj debugger + collapse

--- index.js
+++ index.js
@@ -7,8 +7,22 @@ let debug;
 function init() {
 	debugEl = document.createElement('aside');
 	debugEl.className = 'debug-ui';
-	listEl = document.createElement('ul');
-	debugEl.appendChild(listEl);
+	
+	const label = document.createElement('label');
+	debugEl.appendChild(label);
+	const text = document.createElement('span');
+	text.textContent = 'debug-ui';
+	label.appendChild(text);
+	const panel = getPanel();
+	label.classList.add('collapsible');
+	label.classList.add('collapsed');
+	label.onclick = event => {
+		if (!panel.contains(event.target)) {
+			label.classList.toggle('collapsed');
+		}
+	}
+	label.appendChild(panel);
+	listEl = panel;
 	debug = {};
 	document.body.appendChild(debugEl);
 	function update() {
@@ -36,19 +50,44 @@ function stopPropagation(event) {
 	event.stopPropagation();
 }
 
-export default function(name, getter, setter) {
+function getPanel() {
+	const panel = document.createElement('ul');
+	return panel;
+}
+
+export default function add(name, getter, setter) {
 	lazyInit();
 	const el = document.createElement('li');
 	const label = document.createElement('label');
+	el.appendChild(label);
 	const text = document.createElement('span');
 	text.textContent = name;
 	label.appendChild(text);
 	let update;
 	const initial = getter();
 	const type = typeof initial;
-	if (type === 'object') {
-		// TODO: object debugger
-		// also null
+	if (type === 'object' && initial !== null) {
+		const pList = listEl;
+		const panel = getPanel();
+		label.classList.add('collapsible');
+		label.classList.add('collapsed');
+		label.onclick = () => {
+			label.classList.toggle('collapsed');
+			label.onclick = event => {
+				if (!panel.contains(event.target)) {
+					label.classList.toggle('collapsed');
+				}
+			}
+			listEl = panel;
+			label.appendChild(panel);
+			for (let k in initial) {
+				if (initial.hasOwnProperty && initial.hasOwnProperty(k)) {
+					add(k, () => initial[k], v => initial[k] = v);
+				}
+			}
+			listEl = pList;
+		}
+		listEl.appendChild(el);
 		return;
 	}
 	let prev;
@@ -82,12 +121,18 @@ export default function(name, getter, setter) {
 		if (!setter) {
 			input.readOnly = input.disabled = true;
 		}
+	} else if (type === 'object' || type === 'undefined' || type === 'symbol') {
+		// null
+		input.type = 'text';
+		input.onchange = ({ currentTarget: { value } }) => setter(value);
+		input.onkeydown = input.onkeyup = stopPropagation;
+		update = current => {};
+		input.readOnly = input.disabled = true;
 	} else {
 		throw new Error(`Cannot debug "${name}" due to unsupported type "${type}"`);
 	}
 	update(initial);
 	label.appendChild(input);
-	el.appendChild(label);
 
 	const entry = debug[name] = {
 		update: () => {
